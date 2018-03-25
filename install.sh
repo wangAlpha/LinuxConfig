ff519cdc03380755b4cf4d0b9869779236e13fed
wang <472146630@qq.com>
2018-03-25 10:00:36 -0400

    Add files via upload
    install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+sudo apt-get update;
+
+sudo apt-get install git -y;
+sudo apt-get install vim -y;
+sudo apt-get install curl -y;
+
+sudo apt-get install mysql-client mysql-server -y;
+sudo apt-get install redis -y;
+sudo apt-get install build-essential -y;
+sudo apt-get install llvm -y;
+sudo apt-get install golang -y;
+sudo apt-get install gocode -y;
+sudo apt-get install python3 -y;
+sudo apt-get install pylint -y;
+sudo apt-get install mpg123 -y;
+sudoa apt-get install python3-pip -y;
+sudo pip3 install Netease-Musicbox - y;
+sudo apt-get install thunderbird -y;
+sudo apt-get install jq -y;
+sudo pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
+sudo snap install pycharm-community --classic;
+
+sudo apt-get install zsh;
+sudo apt-get install tlp tlp-rdw -y;
+sudo apt-get install gnome-tweak-tool;
+sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sudo sh spf13-vim.sh
+# sudo apt-get install shadowsocks-qt5 -y;
+# sudo apt-get install docker -y;
+sudo apt-get update;
+sudo apt-get upgrade -y;
+sudo apt-get dist-upgrade -y;
