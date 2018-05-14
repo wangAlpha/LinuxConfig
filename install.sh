6a2c7cadcb7ee00dee9cfe69790dcaadd2220e09
wang <472146630@qq.com>
2018-05-14 12:28:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 sudo apt-get update;
-
 sudo apt-get install git -y;
 sudo apt-get install vim -y;
 sudo apt-get install curl -y;
@@ -9,6 +8,7 @@ sudo apt-get install mysql-client mysql-server -y;
 sudo apt-get install redis -y;
 sudo apt-get install build-essential -y;
 sudo apt-get install llvm -y;
+sudo apt-get install clang-6.0 -y;
 sudo apt-get install golang -y;
 sudo apt-get install gocode -y;
 sudo apt-get install python3 -y;
@@ -16,6 +16,8 @@ sudo apt-get install pylint -y;
 sudo apt-get install mpg123 -y;
 sudoa apt-get install python3-pip -y;
 sudo pip3 install Netease-Musicbox - y;
+
+sudo apt-get install shadowsocks -y;
 sudo apt-get install thunderbird -y;
 sudo apt-get install jq -y;
 sudo pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
@@ -23,13 +25,21 @@ sudo snap install pycharm-community --classic;
 sudo snap install snapd -y;
 sudo snap install electronic-wechat；
 
-sudo apt-get install zsh;
+sudo apt-get install zsh -y;
 sudo apt-get install tlp tlp-rdw -y;
-sudo apt-get install gnome-tweak-tool;
+sudo tlp start;
+sudo apt-get install gnome-tweak-tool -y;
 sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sudo sh spf13-vim.sh
-# sudo apt-get install shadowsocks-qt5 -y;
-# sudo apt-get install docker -y;
 sudo apt-get update;
 sudo apt-get upgrade -y;
 sudo apt-get dist-upgrade -y;
+
+sudo curl https://sh.rustup.rs -sSf | sh;
+cargo install racer;
+rustup component add rust-src;
+cargo +nighty install clippy --force;
+rustup component add rustfmt-preview;
+
+sudo apt-get install --no-install-recommends gdm3 -y;
+sudo apt-get install chromium-browser -y;
+sudo apt-get remove ligthdm -y;                                           
